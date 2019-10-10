8444ed7fc178067f6504d8f402dbd3a1398ba58a
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-10-10 23:15:31 +0300

    2nd commit

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,23 @@ gulp.task('pug', function() {
 
 gulp.task('sass', function() {
   return gulp.src('app/styles/sass/style.sass')
-        .pipe(gulpPlugin.sass())
+        .pipe(gulpPlugin.sourcemaps.init())
+        .pipe(gulpPlugin.sass({}))
+        .pipe(gulpPlugin.autoprefixer({}))
+        .on("error", gulpPlugin.notify.onError({
+          message: "Error: <%= error.message %>",
+          title: "Error in style"
+        }))
+        .pipe(gulpPlugin.sourcemaps.write())
         .pipe(gulp.dest('public/styles/'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('app/assets/pug/**/*.pug', gulp.series('pug'));
+  gulp.watch('app/styles/sass/*.sass', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series(
+  gulp.parallel('pug', 'sass'),
+  'watch'
+));
