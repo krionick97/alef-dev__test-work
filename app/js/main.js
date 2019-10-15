cca559d783f85642b10dcd20c50371f27f2fbff8
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-10-15 22:24:49 +0300

    button click

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
+'use strict'
 $(document).ready(function() {
+  /* Menu button options */
+  const menuButton = document.querySelector('.header__button-menu');
+  const menuBar = document.querySelector('.section__menu');
+  menuButton.addEventListener('click', function() {
+    menuBar.classList.toggle('section__menu-disable');
+    setTimeout(function() {
+      if (menuBar.hidden === false) { menuBar.hidden = true; }
+      else { menuBar.hidden = false; }
+    }, 400)
+  });
+  /* -------------------------- */
+
+
+
 
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,18 @@
 'use strict'
 $(document).ready(function() {
-
-  /* Responsive for mobile menu bar */
-  // define the width of the window
-  const body = document.querySelector('body');
-  body.setAttribute('data-winWidth', window.innerWidth);
-
-  window.addEventListener('resize', function() {    
-    let winWidth = this.innerWidth;
-    body.setAttribute('data-winWidth', winWidth);
-    mobileMenu(winWidth);
+  /* Menu button options */
+  const menuButton = document.querySelector('.header__button-menu');
+  const menuBar = document.querySelector('.section__menu');
+  menuButton.addEventListener('click', function() {
+    menuBar.classList.toggle('section__menu-disable');
+    setTimeout(function() {
+      if (menuBar.hidden === false) { menuBar.hidden = true; }
+      else { menuBar.hidden = false; }
+    }, 400)
   });
+  /* -------------------------- */
 
-  const row1 = document.querySelector('.section__row1');
-  const menuBar = document.querySelector('.section__menu'); // define the menu bar
-  const leftBlock = document.querySelector('.section__leftBlock');
-  const centerBlock = document.querySelector('.section__centerBlock');
-  const rightBlock = document.querySelector('.section__rightBlock');
-  const div = document.createElement('div');
-  div.classList.add('row', 'section__row2');  // the div where blocks are going to be wrapped in
 
-  // function to make the menu bar mobile
-  function mobileMenu(width) {
-    if (width < 1024) {
-      div.append(leftBlock);
-      div.append(centerBlock);
-      div.append(rightBlock);
-      menuBar.after(div);
-      menuBar.classList.remove('col-3');
-      menuBar.classList.add('col');
-    }
-    if (width >= 1024) {
-      if (div) {
-        div.remove();
-        row1.append(leftBlock);
-        row1.append(centerBlock);
-        row1.append(rightBlock);
-        menuBar.classList.remove('col');
-        menuBar.classList.add('col-3');
-      }
-    }
-  }
-  mobileMenu(window.innerWidth);
+
+
 });
