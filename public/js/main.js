280e4a2b358404325de67819fc3a7df573884a85
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-10-18 18:25:50 +0300

    correction

--- public/js/main.js
+++ public/js/main.js
@@ -79,6 +79,7 @@ $(document).ready(function() {
     }
   }
   menuBarTransform(window.innerWidth);
-  /* ------------------------------------------------------- */
+  /* ------------------------------------------------------- end of mobile transforming */
+  
 
 });
